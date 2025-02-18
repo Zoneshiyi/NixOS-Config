d9d5de0bd4d23d2e013bc389e7d71ea9ecb4238d
Zoneshiyi <zoneshiyi@163.com>
2025-02-18 15:10:33 +0800

    Set XDG user dirs

--- modules/home/desktop.nix
+++ modules/home/desktop.nix
@@ -2,10 +2,27 @@
   pkgs,
   pkgs-24_11,
   inputs,
+  userhome,
   ...
 }:
 {
   wayland.windowManager.hyprland.systemd.enable = false;
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    desktop = null;
+    publicShare = null;
+    templates = null;
+    documents = "${userhome}/Documents";
+    download = "${userhome}/Downloads";
+    music = "${userhome}/Music";
+    pictures = "${userhome}/Pictures";
+    videos = "${userhome}/Videos";
+    extraConfig = {
+      XDG_CODES_DIR = "${userhome}/Codes";
+      XDG_TMP_DIR = "${userhome}/Tmp";
+    };
+  };
   home.packages = with pkgs; [
     hyprsunset
     pyprland
