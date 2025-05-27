ecadab4cd483505fa37862b835db63e08afa2d2a
Zoneshiyi <zoneshiyi@163.com>
2025-05-27 14:12:40 +0800

    1. Add udev rules for 0bda:1a2b 2. Update Hyprland conf

--- modules/system/bluetooth.nix
+++ modules/system/bluetooth.nix
@@ -1,4 +1,8 @@
 {
+  pkgs,
+  ...
+}:
+{
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
@@ -11,4 +15,8 @@
   };
 
   services.blueman.enable = true;
+
+  services.udev.extraRules = ''
+    ATTR{idVendor}=="0bda", ATTR{idProduct}=="1a2b", RUN+="${pkgs.usb-modeswitch}/bin/usb_modeswitch -v 0bda -p 1a2b -V 0bda -P b851 -K"
+  '';
 }
